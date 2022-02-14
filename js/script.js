f940983c984432c65533ea5804cb38bd23e258d1
Najim vuiya <vuiyanajim4900@gmail.com>
2022-02-14 21:45:47 +0600

    three file update now

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
 'use strict';
 console.log('Hello world this is my my first github project');
 console.log('I love my allah.');
+
+const myForm = document.getElementsByTagName('form');
+console.log(myForm);
+
